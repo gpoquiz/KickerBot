92a42b2bb34f6eddbd39a04a3d8ca7214c4a5de8
GPO <gpoquiz@gmail.com>
2018-09-16 17:35:38 -0500

    added a config dependency

--- app.js
+++ app.js
@@ -4,12 +4,14 @@
 
 // Import the discord.js module
 const Discord = require('discord.js');
+const config = require('./config.json');
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
 // The token of your bot - https://discordapp.com/developers/applications/me
-const token = ''token here';
+// Log our bot in
+client.login(config.token);
 
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
@@ -18,7 +20,7 @@ client.on('ready', () => {
 });
 
 // Create an event listener for messages
-client.on('message', message => {
+client.on('message',async message => {
   // If the message is "ping"
   if (message.content === 'ping') {
     // Send "pong" to the same channel
@@ -53,8 +55,6 @@ client.on('message', message => {
 								newChannel.delete().catch(O_o=>{}); 	
 							});
 							
-						
-						
 					});
 					
 				break;
@@ -86,10 +86,62 @@ client.on('message', message => {
 				clearInterval(timer);
                 channel.send('Pong!');
 				break;
+			case 'narwhal':
+				var newChannel;
+				var punteeName = (args[0].substring(args[0].length - 19, args[0].length - 1));
+				var puntee = server.members.get(punteeName);
+					channel.send( 'I have a song for you ' + args[0]);
+					
+					channel.send( '?stop').then((message) =>
+					{
+						channel.send( '?play https://www.youtube.com/watch?v=GcYVCvBq0FY').then((message) =>
+						{
+							
+						});
+					});
+					
+				break;
+				
+			case 'mock':
+				var newChannel;
+				var punteeName = (args[0].substring(args[0].length - 19, args[0].length - 1));
+				var puntee = server.members.get(punteeName);
+				//	channel.send("" + args[0].substring(3, args[0].length - 1));
+				
+				var lastMsg = puntee.lastMessage.content;
+				var newMsg = '';
+				for (var i = 0; i < lastMsg.length; i++)
+				{
+					newMsg += (i&1) ? (lastMsg.charAt(i).toUpperCase()) : (lastMsg.charAt(i).toLowerCase())
+				}
+				channel.send(newMsg);
+					
+				break;
+			case 'thisissosad':
+				voiceChannel = message.member.voiceChannel;
+				voiceChannel.join()
+				.then(connection => 
+				{
+					console.log('Connected!')
+				const dispatcher = connection.playFile('Despacito.mp3');
+				dispatcher.setVolume(0.1) 
+				dispatcher.on('finish', () => {
+				  console.log('Finished playing!');
+					});
+				})
+				.catch(console.error);;
+
+				break;
+			case 'join':
+				voiceChannel = message.member.voiceChannel;
+				voiceChannel.join()
+				.then(connection => console.log('Connected!'))
+				.catch(console.error);
+				break;
+
+			
+				
 				
          }
      }
 });
-
-// Log our bot in
-client.login(token);
